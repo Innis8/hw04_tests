89c9acbb4bd79ea993552b2efb11f774fabc59a0
Denis Mushits <horo-braven@yandex.ru>
2022-03-08 19:11:22 +0300

    added comment model and form, done migrations needed

--- yatube/posts/forms.py
+++ yatube/posts/forms.py
@@ -1,8 +1,18 @@
 from django import forms
-from posts.models import Post
+from posts.models import Post, Comment
 
 
 class PostForm(forms.ModelForm):
     class Meta:
         model = Post
         fields = ('text', 'group', 'image')
+
+
+class CommentForm(forms.ModelForm):
+
+    class Meta:
+        model = Comment
+        fields = ['text']
+        widgets = {
+            'text': forms.TextInput(attrs={'placeholder': 'Комментарий'}),
+        }
